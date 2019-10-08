904c728e0333fd3fc9078619067c687d2a7c21ba
winstonitgf <aa71435723@gmail.com>
2019-10-08 21:36:36 +0800

    新增chart-legend區塊說明

--- src/app/jquery/chart/chart.module.ts
+++ src/app/jquery/chart/chart.module.ts
@@ -6,9 +6,10 @@ import { PreRequestDataComponent } from './pre-request-data/pre-request-data.com
 import { AngularMaterialModule } from 'src/app/_material/angular-material.module';
 import { ChartInitComponent } from './javascript/chart-init/chart-init.component';
 import { ChartTitleComponent } from './javascript/chart-title/chart-title.component';
+import { ChartLegendComponent } from './javascript/chart-legend/chart-legend.component';
 
 @NgModule({
-  declarations: [ChartComponent, PreRequestHtmlComponent, PreRequestDataComponent, ChartInitComponent, ChartTitleComponent],
+  declarations: [ChartComponent, PreRequestHtmlComponent, PreRequestDataComponent, ChartInitComponent, ChartTitleComponent, ChartLegendComponent],
   imports: [CommonModule, AngularMaterialModule],
   exports: [ChartComponent]
 })
---
+++ src/app/jquery/chart/javascript/chart-legend/chart-legend.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chart-legend',
+  templateUrl: './chart-legend.component.html',
+  styleUrls: ['./chart-legend.component.scss']
+})
+export class ChartLegendComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
