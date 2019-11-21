aa9f3a0fc999cf82ad4796b42a24b81f2522df28
winstonitgf <aa71435723@gmail.com>
2019-11-22 00:17:29 +0800

    新增Angular Grid內容

--- src/app/angular/grid/grid.module.ts
+++ src/app/angular/grid/grid.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { AngularGridComponent } from './grid.component';
 import { AngularMaterialModule } from 'src/app/_material/angular-material.module';
+import { HtmlComponent } from './components/html/html.component';
 
 @NgModule({
-  declarations: [AngularGridComponent],
+  declarations: [AngularGridComponent, HtmlComponent],
   imports: [AngularMaterialModule],
   exports: [AngularGridComponent]
 })
