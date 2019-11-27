029d0fbae0e3357e86be5f4422f7f3692fc035b4
winstonitgf <aa71435723@gmail.com>
2019-11-27 22:59:17 +0800

    新增modeule的程式教學碼

--- src/app/angular/grid/grid.module.ts
+++ src/app/angular/grid/grid.module.ts
@@ -3,9 +3,10 @@ import { AngularGridComponent } from './grid.component';
 import { AngularMaterialModule } from 'src/app/_material/angular-material.module';
 import { HtmlComponent } from './components/html/html.component';
 import { TsComponent } from './components/ts/ts.component';
+import { ModuleComponent } from './components/module/module.component';
 
 @NgModule({
-  declarations: [AngularGridComponent, HtmlComponent, TsComponent],
+  declarations: [AngularGridComponent, HtmlComponent, TsComponent, ModuleComponent],
   imports: [AngularMaterialModule],
   exports: [AngularGridComponent]
 })
