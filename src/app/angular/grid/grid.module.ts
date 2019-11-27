68103504681d8db6a5d113c6b45c4048ee73791b
winstonitgf <aa71435723@gmail.com>
2019-11-27 23:39:17 +0800

    新增service&model程式教學碼(未完成)

--- src/app/angular/grid/grid.module.ts
+++ src/app/angular/grid/grid.module.ts
@@ -4,9 +4,11 @@ import { AngularMaterialModule } from 'src/app/_material/angular-material.module
 import { HtmlComponent } from './components/html/html.component';
 import { TsComponent } from './components/ts/ts.component';
 import { ModuleComponent } from './components/module/module.component';
+import { ServiceComponent } from './components/service/service.component';
+import { ModelComponent } from './components/model/model.component';
 
 @NgModule({
-  declarations: [AngularGridComponent, HtmlComponent, TsComponent, ModuleComponent],
+  declarations: [AngularGridComponent, HtmlComponent, TsComponent, ModuleComponent, ServiceComponent, ModelComponent],
   imports: [AngularMaterialModule],
   exports: [AngularGridComponent]
 })
