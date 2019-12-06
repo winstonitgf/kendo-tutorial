47db12d323cee5fd45d1697b7a455e480ddea8f5
Winston 盧文松 <aa71435723@gmail.com>
2019-12-06 17:24:19 +0800

    新增component ts html module

---
+++ src/app/angular/scheduler/components/ts/ts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ts',
+  templateUrl: './ts.component.html',
+  styleUrls: ['./ts.component.scss']
+})
+export class TsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/angular/scheduler/scheduler.module.ts
+++ src/app/angular/scheduler/scheduler.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { AngularSchedulerComponent } from './scheduler.component';
 import { AngularMaterialModule } from 'src/app/_material/angular-material.module';
+import { HtmlComponent } from './components/html/html.component';
+import { TsComponent } from './components/ts/ts.component';
+import { ModuleComponent } from './components/module/module.component';
 
 @NgModule({
-  declarations: [AngularSchedulerComponent],
+  declarations: [AngularSchedulerComponent, HtmlComponent, TsComponent, ModuleComponent],
   imports: [
     AngularMaterialModule
   ],
