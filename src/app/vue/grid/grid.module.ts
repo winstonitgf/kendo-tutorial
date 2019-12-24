26b92cb09038860d4af86314e4ff0b86c71942fc
Wade <czw.larvata@gmail.com>
2019-12-24 23:33:15 +0800

    新增vue-grid頁面

--- src/app/vue/grid/grid.module.ts
+++ src/app/vue/grid/grid.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VueGridComponent } from './grid.component';
+import { FilterComponent } from './components/filter/filter.component';
+import { GroupComponent } from './components/group/group.component';
+import { SortComponent } from './components/sort/sort.component';
+import { EditComponent } from './components/edit/edit.component';
+import { ExportComponent } from './components/export/export.component';
+import { PageComponent } from './components/page/page.component';
+import { AngularMaterialModule } from 'src/app/_material/angular-material.module';
+
 
 @NgModule({
-  declarations: [VueGridComponent],
+  declarations: [VueGridComponent, FilterComponent, GroupComponent, SortComponent, EditComponent, ExportComponent, PageComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    AngularMaterialModule
   ]
 })
 export class GridModule { }
