5e939c5b791099e0ba2a18927f342d2de5e2261a
Winston 盧文松 <aa71435723@gmail.com>
2020-01-20 17:49:45 +0800

    加入angular-chart 頁面描述

--- src/app/angular/chart/chart.module.ts
+++ src/app/angular/chart/chart.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AngularChartComponent } from './chart.component';
+import { AngularMaterialModule } from 'src/app/_material/angular-material.module';
 
 @NgModule({
   declarations: [AngularChartComponent],
   imports: [
-    CommonModule
+    AngularMaterialModule
   ],
   exports:[
     AngularChartComponent
