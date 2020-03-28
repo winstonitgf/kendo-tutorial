ae696518df59f5c061319b07a9bd1c7015541730
Wade <czw.larvata@gmail.com>
2020-03-28 15:46:06 +0800

    新增Vue行事曆頁面

--- src/app/vue/scheduler/scheduler.module.ts
+++ src/app/vue/scheduler/scheduler.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VueSchedulerComponent } from './scheduler.component';
+import { AngularMaterialModule } from 'src/app/_material/angular-material.module';
+
 
 @NgModule({
   declarations: [VueSchedulerComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    AngularMaterialModule
   ]
 })
 export class SchedulerModule { }
