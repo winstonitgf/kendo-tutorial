092b0df3dcef6a7d5f0f72f05057b2f947b762e2
Wade <czw.larvata@gmail.com>
2020-04-09 17:44:20 +0800

    新增Vue表單頁面

---
+++ src/app/vue/form/components/date-inputs/date-inputs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-date-inputs',
+  templateUrl: './date-inputs.component.html',
+  styleUrls: ['./date-inputs.component.scss']
+})
+export class DateInputsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/vue/form/form.module.ts
+++ src/app/vue/form/form.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VueFormComponent } from './form.component';
 import { AngularMaterialModule } from 'src/app/_material/angular-material.module';
+import { InputsComponent } from './components/inputs/inputs.component';
+import { DateInputsComponent } from './components/date-inputs/date-inputs.component';
+import { UploadComponent } from './components/upload/upload.component';
+import { DropdownComponent } from './components/dropdown/dropdown.component';
 
 @NgModule({
-  declarations: [VueFormComponent],
+  declarations: [VueFormComponent, InputsComponent, DateInputsComponent, UploadComponent, DropdownComponent],
   imports: [
     CommonModule,
     AngularMaterialModule
